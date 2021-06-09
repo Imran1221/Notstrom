15fa5f4f537069e847ade690dfd77d114dad8c22
Imran Nteli <imranntelichalil@hotmail.de>
2021-06-09 19:06:28 +0200

    all Validierungen without ort

--- js/bestellformular.js
+++ js/bestellformular.js
@@ -23,27 +23,52 @@ function berechnePreis() {
 function pruefeFormular(evt) {
     let strFehler = document.getElementById("strValidierung");
     let plzFehler = document.getElementById("plzValidierung");
+    let emailFehler = document.getElementById("emailValidierung");
+    let anredeFehler = document.getElementById("anredeValidierung");
+    let agbFehler = document.getElementById("agbValidierung");
+    strFehler.innerHTML = "";
+    plzFehler.innerHTML = "";
+    emailFehler.innerHTML = "";
+    anredeFehler.innerHTML = "";
+    agbFehler.innerHTML = "";
     if (document.bestellformular.str.value == "") {
-        strFehler.style.display = "block"
-        strFehler.innerHTML = "Bitte geben Sie eine valide Straße ein!";
+        strFehler.innerHTML = "</br>Bitte geben Sie eine valide Straße ein!";
         strFehler.style.color = "red";
         evt.preventDefault();
     }
-    if (document.bestellformular.plz.value.length < 4 || document.bestellformular.plz.value.length > 5) {
-        plzFehler.style.display = "block";
-        plzFehler.innerHTML = "Bitte geben Sie eine vier- oder fünfstellige Postleitzahl ein!";
+
+    if (isNaN(document.bestellformular.plz.value)) {
+        plzFehler.innerHTML = "</br>Die Postleitzahl darf nur ganze Zahlen beinhalten!";
+        plzFehler.style.color = "red";
+        evt.preventDefault();
+    } else if (document.bestellformular.plz.value.length < 4 || document.bestellformular.plz.value.length > 5) {
+        plzFehler.innerHTML = "</br>Bitte geben Sie eine vier- oder fünfstellige Postleitzahl ein!";
         plzFehler.style.color = "red";
         evt.preventDefault();
     }
-    if (isNan(bestellformular.plz.value)) {
-        plzFehler.innerHTML = "Bitte geben Sie eine vier- oder fünfstellige Postleitzahl-Nummer ein!";
+
+    let testValidEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^< () \[\]\\.,;: \s@"]+)*)|("."))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zAZ]{2,}))$/;
+    if (!testValidEmail.test(document.bestellformular.email.value)) {
+        emailFehler.innerHTML = "</br>Bitte geben Sie einge gültige Email-Adresse ein!";
+        emailFehler.style.color = "red";
+        evt.preventDefault();
     }
-    let testValidEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{ 1, 3 } \.[0 - 9]{ 1, 3 } \.[0 - 9]{ 1, 3 } \.[0 - 9]{ 1, 3 }])|(([a - zA - Z\-0 - 9] +\.) +[a - zAZ]{2,})) $ /;
-    if (testValidEmail.test(bestellformular.email.value)) {
-        alert("huhu");
+    if (document.bestellformular.anrede.value == "") {
+        anredeFehler.innerHTML = "</br>Bitte geben Sie Ihren Geschlecht ein!";
+        anredeFehler.style.color = "red";
+        evt.preventDefault();
     }
-    if (strFehler.style.display == "none" || plzFehler.style.display == "none") {
-        alert("alles richtig!");
+    if (document.getElementById("agbChecker").checked == false) {
+        agbFehler.innerHTML = "</br>Bitte akzeptieren Sie die AGB!";
+        agbFehler.style.color = "red";
+        evt.preventDefault();
+    }
+
+    if (strFehler.innerHTML == "" && plzFehler.innerHTML == "" && emailFehler.innerHTML == "" && anredeFehler.innerHTML == "") {
+        let bestelltText = document.getElementById("bestellt");
+        bestelltText.innerHTML = "Sie haben erfolgreich bestellt";
+        bestelltText.style.color = "green";
+        evt.preventDefault();
     }
 }
 function oeffneInfo() {
@@ -53,10 +78,10 @@ function oeffneInfo() {
     let paketdienst = document.bestellformular.paketdienst.value;
     console.log(paketdienst);
     if (paketdienst == "DHL") {
-        window.open("http://www.dhl.de", "_blank");
+        window.open("http://www.dhl.de", "_blank", "width=500,height=500");
     } else if (paketdienst == "UPS") {
-        window.open("https://www.ups.com", "_blank");
+        window.open("https://www.ups.com", "_blank", "width=500,height=500");
     } else {
-        window.open("https://www.hermes.com/", "_blank");
+        window.open("https://www.hermes.com/", "_blank", "width=500,height=500");
     }
 }
